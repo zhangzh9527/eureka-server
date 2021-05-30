6617fbae978c0d01c621b5d56573325e03e42a15
zhangzh <zhangzh1010@163.com>
2021-05-30 13:24:58 +0800

    20210530--注册中心init

---
+++ src/main/java/com/example/boot/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.boot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
